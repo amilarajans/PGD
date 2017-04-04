f18e2bc65432562845d155aeb180cbe145d3380a
manoj <manoj@smartb4b.nl>
2017-04-04 22:25:10 +0530

    Student Payments

--- src/main/webapp/scripts/controllers/transactions/paymentController.js
+++ src/main/webapp/scripts/controllers/transactions/paymentController.js
@@ -1,9 +1,15 @@
 /**
  * Created by Manoj Janaka on 14-11-2016.
  */
-activitiAdminApp.controller('paymentController', ['$rootScope', '$scope', '$http', 'toastr', '$uibModal', '$uibModalInstance', 'element','selectedStudent',
-    function ($rootScope, $scope, $http, toastr, $uibModal, $uibModalInstance, element,selectedStudent) {
+activitiAdminApp.controller('paymentController', ['$rootScope', '$filter', '$scope', '$http', 'toastr', '$uibModal', '$uibModalInstance', 'element','selectedStudent',
+    function ($rootScope, $filter, $scope, $http, toastr, $uibModal, $uibModalInstance, element,selectedStudent) {
 
         $scope.element = element;
         $scope.selectedStudent = selectedStudent;
+        $scope.paymentObj = {};
+        $scope.paymentObj.paymentDate = $filter("date")(Date.now(), 'yyyy-MM-dd');
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
     }]);
